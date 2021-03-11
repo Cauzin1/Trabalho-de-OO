77a1555a7d4ec4ecf3dae979c11848e30f08431b
Cauzin1 <thomas.causin@estudante.ufjf.br>
2021-03-11 20:47:17 -0300

    simplesmente o lixo do netbeans nao queria compilar mais, vamos de intellij

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,84 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author giova
+ */
+
+public class Cliente {
+
+    private String nome;
+    private String sexo;
+    private String email;
+    private String cpf;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Cliente(String nome, String sexo, String email, String cpf) {
+        //construtor
+        this.nome = nome;
+        this.sexo = sexo;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+ 
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,28 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author giova
+ */
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class main {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        JTextArea j = new JTextArea();
+        l.setLocationRelativeTo(null); // abre a interface no meio da tela
+        l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // fechar programa no x
+        l.setSize(250, 250); //tamanho janela
+        l.setVisible(true); //chamar a interface
+        j.setLineWrap(true);//pesquisei e achei esse comando para alinhar texto
+
+    }
+//euodeionetbeans
+}
